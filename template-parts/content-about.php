8d27dbd009b0e5231c3297384b1ed431a1551ac0
Mike Oberdick <mikeoberdick@gmail.com>
2017-08-01 10:40:13 -0400

    Contact Page & Updates

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -15,26 +15,45 @@ $martin = get_field('martin_headshot');
 $barry = get_field('barry_headshot');
 ?>
 
-<div id = "aboutBios" class="row">
+<div id = "aboutBios">
+
+<div class = "bio row">
 	<div class="col-sm-12 col-md-4">
-		<img class="rounded-circle mb-3" src="<?php echo $sean['url']; ?>" title = "<?php echo $sean['title']; ?>" alt="<?php echo $sean['alt']; ?>">
+		<img class="rounded-circle mb-3" src="<?php echo $sean['url']; ?>" title = "<?php echo $sean['title']; ?>" alt="<?php echo $sean['alt']; ?>">	
+	</div>
+
+	<div class="col-sm-12 col-md-8 text-right">
 		<h3>Sean Gilshannon</h3>
 		<h5><?php the_field('sean_title'); ?></h5>
 		<p><?php the_field('sean_bio'); ?></p>
-	</div><!-- .col-sm-12 col-md-4 -->
-	<div class="col-sm-12 col-md-4">
-		<img class="rounded-circle mb-3" src="<?php echo $martin['url']; ?>" title = "<?php echo $martin['title']; ?>" alt="<?php echo $martin['alt']; ?>">
+	</div>
+</div><!-- .bio -->
+
+<div class = "bio row">
+	<div class="col-sm-12 col-md-8">
 		<h3>Martin Kee</h3>
 		<h5><?php the_field('martin_title'); ?></h5>
 		<p><?php the_field('martin_bio'); ?></p>
 	</div><!-- .col-sm-12 col-md-4 -->
+
+	<div class="col-sm-12 col-md-4">
+		<img class="rounded-circle mb-3" src="<?php echo $martin['url']; ?>" title = "<?php echo $martin['title']; ?>" alt="<?php echo $martin['alt']; ?>">
+	</div>
+</div><!-- .bio -->
+
+<div class = "bio row">
 	<div class="col-sm-12 col-md-4">
 		<img class="rounded-circle mb-3" src="<?php echo $barry['url']; ?>" title = "<?php echo $barry['title']; ?>" alt="<?php echo $barry['alt']; ?>">
+	</div><!-- .col-sm-12 col-md-4 -->
+
+	<div class="col-sm-12 col-md-8">
 		<h3>Barry Smith</h3>
 		<h5><?php the_field('barry_title'); ?></h5>
 		<p><?php the_field('barry_bio'); ?></p>
-	</div><!-- .col-sm-12 col-md-4 -->
-</div><!-- .row -->
+	</div>
+</div><!-- .bio -->
+
+</div><!-- #aboutBios -->
 </div><!-- .container -->
 
 </div><!-- #about -->
